862ed7df5701de577fda2fb9ec72ae1069ad4dfb
Kamrab <kamran@gmail.com>
2023-08-22 15:13:49 +0500

    8/22/2023

--- app/src/main/java/com/example/toastlibrary/MainActivity.kt
+++ app/src/main/java/com/example/toastlibrary/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.toastlibrary
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.myfirstlibrary.ToastClass
 import com.example.myfirstlibrary.ToastClass.showToast
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
